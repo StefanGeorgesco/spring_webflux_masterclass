12fd5d97e8a4ad1a484c209d69cfee348988c402
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-07-22 20:12:02 +0200

    first commit

---
+++ src/main/java/fr/stefangeorgesco/spring_webflux_masterclass/SpringWebfluxMasterclassApplication.java
@@ -0,0 +1,13 @@
+package fr.stefangeorgesco.spring_webflux_masterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxMasterclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxMasterclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/stefangeorgesco/spring_webflux_masterclass/SpringWebfluxMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.stefangeorgesco.spring_webflux_masterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxMasterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
