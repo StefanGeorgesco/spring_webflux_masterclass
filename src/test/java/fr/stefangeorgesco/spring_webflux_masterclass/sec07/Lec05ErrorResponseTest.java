94834d42cd47201ebeb91fbaec3fbda0dfbe3767
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-07-31 19:29:39 +0200

    sec07.Lec05ErrorResponseTest: exchange

--- src/test/java/fr/stefangeorgesco/spring_webflux_masterclass/sec07/Lec05ErrorResponseTest.java
+++ src/test/java/fr/stefangeorgesco/spring_webflux_masterclass/sec07/Lec05ErrorResponseTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @SuppressWarnings("LoggingSimilarMessage")
@@ -50,5 +52,44 @@ class Lec05ErrorResponseTest extends AbstractWebClient {
                 .as(StepVerifier::create)
                 .expectComplete()
                 .verify();
+    }
+
+    @Test
+    void exchange_NoError() {
+        client.get()
+                .uri("/lec05/calculator/{a}/{b}", 20, 3)
+                .header("operation", "/")
+                .exchangeToMono(this::decode)
+                .doOnNext(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void exchange_Error() {
+        client.get()
+                .uri("/lec05/calculator/{a}/{b}", 20, 3)
+                .header("operation", "@")
+                .exchangeToMono(this::decode)
+                .doOnNext(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    private Mono<CalculatorResponse> decode(ClientResponse clientResponse) {
+//        clientResponse.cookies()
+//        clientResponse.headers()
+        log.info("status code: {}", clientResponse.statusCode());
+        if (clientResponse.statusCode().isError()) {
+            return clientResponse.bodyToMono(ProblemDetail.class)
+                    .doOnNext(problemDetail -> log.error("Error response detail: {}", problemDetail))
+                    .then(Mono.empty());
+        } else {
+            return clientResponse.bodyToMono(CalculatorResponse.class);
+        }
     }
 }
