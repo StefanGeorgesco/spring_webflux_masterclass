f19345be0578ea6fb244f154a640a546c1b3075e
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-08-04 10:19:54 +0200

    sec07: add Lec09ExchangeFilterTest

---
+++ src/test/java/fr/stefangeorgesco/spring_webflux_masterclass/sec07/Lec09ExchangeFilterTest.java
@@ -0,0 +1,55 @@
+package fr.stefangeorgesco.spring_webflux_masterclass.sec07;
+
+import fr.stefangeorgesco.spring_webflux_masterclass.sec07.dto.Product;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
+
+import java.util.UUID;
+
+class Lec09ExchangeFilterTest extends AbstractWebClient {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec09ExchangeFilterTest.class);
+
+    private final WebClient client = createWebClient(builder ->
+            builder
+                    .filter(tokenGenerator())
+                    .filter(logRequest()));
+
+    @Test
+    void exchangeFilter() {
+        for (int i = 1; i <= 5; i++) {
+            client.get()
+                    .uri("/lec09/product/{id}", i)
+                    .retrieve()
+                    .bodyToMono(Product.class)
+                    .doOnNext(print())
+                    .then()
+                    .as(StepVerifier::create)
+                    .expectComplete()
+                    .verify();
+        }
+    }
+
+    private ExchangeFilterFunction tokenGenerator() {
+        return (request, next) -> {
+            var token = UUID.randomUUID().toString().replace("-", "");
+            log.info("generated token: {}", token);
+            var modifiedRequest = ClientRequest.from(request)
+                    .headers(headers -> headers.setBearerAuth(token))
+                    .build();
+            return next.exchange(modifiedRequest);
+        };
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return (request, next) -> {
+            log.info("Request: {} {}", request.method(), request.url());
+            return next.exchange(request);
+        };
+    }
+}
